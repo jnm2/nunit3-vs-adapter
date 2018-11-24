c006744ab5dec826f6b54d2372858fa2906db6c9
jnm2 <me@jnm2.com>
2018-11-24 12:45:43 -0500

    Test that NUnit.Engine is not overwritten in test projects

---
+++ tests/Referencing NUnit.Engine/ReferencingNUnitEngineTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace Referencing_NUnit_Engine
+{
+    public class ReferencingNUnitEngineTests
+    {
+        [Test]
+        public void UsesCorrectVersionOfNUnitEngine()
+        {
+            const string versionNotUsedByNUnitAdapter = "3.7.0";
+
+            var assembly = typeof(NUnit.Engine.NUnitEngineException)
+#if NETCOREAPP1_0
+                .GetTypeInfo()
+#endif
+                .Assembly;
+
+            var versionBlock = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            Assert.That(versionBlock.ProductVersion, Is.EqualTo(versionNotUsedByNUnitAdapter));
+        }
+    }
+}
